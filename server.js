bc7c0cd9a9b3ce3568936ead54d47368c25c2551
Pavel <pavel.pochtarev@gmail.com>
2026-02-18 08:14:32 +0300

    Add logging to minimal server

--- server.js
+++ server.js
@@ -4,10 +4,13 @@ const app = express();
 app.use(express.json());
 
 app.get('/', (req, res) => {
+    console.log("GET / called");
     res.send("Minimal server is running!");
 });
 
 app.post('/alice', (req, res) => {
+    console.log("POST /alice called");
+    console.log("Request body:", JSON.stringify(req.body, null, 2));
     res.json({
         response: { text: 'Minimal server response!' },
         version: req.body.version || '1.0'
